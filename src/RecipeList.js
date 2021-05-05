037513a9498a641157161418b30a8c2a6cfed3d3
Ye Shi <zoe.shiye@gmail.com>
2021-05-06 06:12:17 +1000

    added css files

--- src/RecipeList.js
+++ src/RecipeList.js
@@ -3,20 +3,19 @@ import Recipe from "./Recipe";
 
 const RecipeList = ({ recipes }) => {
   return (
-    <>
+    <div className="recipe-list">
       <div>
-        {recipes.map(recipe => {
-          return (
-            <Recipe 
-              key={recipe.id} 
-              {...recipe} 
-            />
-          )
+        {recipes.map((recipe) => {
+          return <Recipe key={recipe.id} {...recipe} />;
         })}
       </div>
-      <button>Add Recipe</button>
-    </>
-  )
+      <div className="recipe-list__add-recipe-btn-container">
+        <button className="btn btn--primary">
+          Add Recipe
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default RecipeList;
