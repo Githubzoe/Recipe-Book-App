de12117dcac90aef8f27dc17c4c4b630c1f95a91
Ye Shi <zoe.shiye@gmail.com>
2021-05-07 21:22:15 +1000

    modified add recipe functionality

--- src/components/App.js
+++ src/components/App.js
@@ -35,13 +35,14 @@ function App() {
   function handleRecipeAdd() {
     const newRecipe = {
       id: uuidv4(),
-      name: "New",
+      name: "",
       servings: 1,
-      cookTime: "1:00",
-      instructions: "Instr.",
-      ingredients: [{ id: uuidv4(), name: "Name", amount: "1 Tbs" }],
+      cookTime: "",
+      instructions: "",
+      ingredients: [{ id: uuidv4(), name: "", amount: "" }],
     };
 
+    setSeletedRecipeId(newRecipe.id)
     setRecipes([...recipes, newRecipe]);
   }
 
